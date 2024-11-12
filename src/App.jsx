897aabbaa395c555f1f7378ea43a3386908cd803
Abhishek Prajapatt <145788814+abhishekprajapati2006@users.noreply.github.com>
2024-11-13 00:30:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,16 @@ import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
 import Footer from './components/Footer';
 
+import backgroundMusic from '../public/iPhoneBgm.mp3';
+
 import * as Sentry from '@sentry/react';
 
 const App = () => {
   return (
     <main className="bg-black">
+      <audio id="audio" loop autoPlay>
+        <source src={backgroundMusic} type="audio/mpeg" />
+      </audio>
       <Navbar />
       <Hero />
       <Highlights />
